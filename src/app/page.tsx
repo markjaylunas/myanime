7ea43f4f7075086c33a2f1a130990ebdb2596ff1
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-09 16:07:51 +0800

    update headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,34 @@ import AnimeCardListSkeleton from "@/components/home/AnimeCardListSkeleton";
 import Popular from "@/components/home/Popular";
 import RecentEpisode from "@/components/home/RecentEpisode";
 import Trending from "@/components/home/Trending";
+import Heading from "@/components/ui/Heading";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <main className="container max-w-5xl mx-auto min-h-screen px-4">
-      <section className="space-y-3 mb-4">
-        <h2 className="text-2xl md:text-3xl font-bold ">Recent Episodes</h2>
+      <section className="space-y-3 mb-8">
+        <Heading order="2xl" className="text-gray-300">
+          Recent Episodes
+        </Heading>
         <Suspense fallback={<AnimeCardListSkeleton />}>
           <RecentEpisode />
         </Suspense>
       </section>
 
-      <section className="space-y-3 mb-4">
-        <h2 className="text-2xl md:text-3xl font-bold ">Trending</h2>
+      <section className="space-y-3 mb-8">
+        <Heading order="2xl" className="text-gray-300">
+          Trending
+        </Heading>
         <Suspense fallback={<AnimeCardListSkeleton />}>
           <Trending />
         </Suspense>
       </section>
 
-      <section className="space-y-3 mb-4">
-        <h2 className="text-2xl md:text-3xl font-bold ">Popular</h2>
+      <section className="space-y-3 mb-8">
+        <Heading order="2xl" className="text-gray-300">
+          Popular
+        </Heading>
         <Suspense fallback={<AnimeCardListSkeleton />}>
           <Popular />
         </Suspense>
