fb0ab2c90663b426a38c84a6955980fb001be476
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-09 15:27:38 +0800

    Fix card overlay

--- src/components/home/AnimeCard.tsx
+++ src/components/home/AnimeCard.tsx
@@ -12,25 +12,15 @@ export default function AnimeCard({ anime }: Props) {
   const title =
     anime.title.english || anime.title.romaji || anime.title.native || anime.id;
   return (
-    <Card
-      radius="lg"
-      className="rounded-lg h-[200px] sm:h-[250px] md:h-[300px] aspect-2/3 bg-transparent select-none hover:cursor-pointer"
-    >
-      {/* <CardHeader className="absolute z-10 top-0 w-full flex justify-end items-start p-2">
-        {anime.subOrDub === "dub" && (
-          <Chip color="secondary" size="sm" radius="lg">
-            DUB
-          </Chip>
-        )}
-      </CardHeader> */}
+    <Card className="relative h-[200px] sm:h-[250px] md:h-[300px] aspect-2/3 bg-transparent select-none hover:cursor-pointer overflow-hidden">
+      <div className="absolute z-10 w-full h-full bg-gradient-to-t from-black  via-transparent to-transparent" />
       <Image
         removeWrapper
         alt={title}
         className="z-0 w-full h-full object-cover"
         src={anime.image}
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
-      <CardFooter className="absolute z-10 bottom-0 p-4 flex justify-center items-start">
+      <CardFooter className="absolute z-20 bottom-0 p-4 flex justify-center items-start">
         <Link href={href}>
           <h4 className="text-white font-bold text-xs md:text-lg line-clamp-3 text-center text-pretty">
             {title}
