651cf9b30d95c60d77014b5461a0b802d27bbf5f
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-10 13:13:43 +0800

    Add home page layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,5 @@
+import Heading from "@/components/ui/Heading";
+import { Spacer } from "@nextui-org/spacer";
 import { ReactNode } from "react";
 
 export default async function HomeLayout({
@@ -16,12 +18,42 @@ export default async function HomeLayout({
   topairing: ReactNode;
 }) {
   return (
-    <main className="container max-w-5xl mx-auto min-h-screen px-4">
+    <main className="container max-w-5xl mx-auto min-h-screen px-8">
       {children}
+
+      <Spacer y={8} />
+
+      <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
+        Popular
+      </Heading>
       {popular}
+
+      <Spacer y={8} />
+
+      <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
+        Recent Episodes
+      </Heading>
       {recentepisodes}
+
+      <Spacer y={8} />
+
+      <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
+        Top Airing
+      </Heading>
       {topairing}
+
+      <Spacer y={4} />
+
+      <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
+        Movies
+      </Heading>
       {movies}
+
+      <Spacer y={8} />
+
+      <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
+        All
+      </Heading>
       {animelist}
     </main>
   );
