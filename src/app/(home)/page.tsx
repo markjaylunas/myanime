9735b283b34ce34a11baaf8b656afe0a1f61f33d
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-10 13:27:17 +0800

    Remove unused referrence

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,34 +2,6 @@ export default async function Home() {
   return (
     <>
       <h1>Makje</h1>
-
-      {/* <section className="space-y-3 mb-8">
-        <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
-          Recent Episodes
-        </Heading>
-        <Suspense fallback={<AnimeCardListSkeleton />}>
-          <RecentEpisode />
-        </Suspense>
-      </section>
-
-      <section className="space-y-3 mb-8">
-        <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
-          Trending
-        </Heading>
-        <Suspense fallback={<AnimeCardListSkeleton />}>
-          <Trending />
-        </Suspense>
-      </section>
-
-      <section className="space-y-3 mb-8">
-        <Heading order="2xl" className="text-gray-700 dark:text-gray-300 ">
-          Popular
-        </Heading>
-        <Suspense fallback={<AnimeCardListSkeleton />}>
-          <Popular />
-        </Suspense>
-      </section>
-       */}
     </>
   );
 }
