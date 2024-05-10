c486650be1e64d318d47e1d01d0ef2a7db7abc39
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-10 21:26:04 +0800

    Refactor for text only

--- src/components/ui/ExpandDescription.tsx
+++ src/components/ui/ExpandDescription.tsx
@@ -5,39 +5,22 @@ import { Toggle } from "./toggle";
 type Props = React.HTMLProps<HTMLParagraphElement> & {
   maxChars?: number;
   description: string;
-  isHtmlTemplate?: boolean;
 };
 
 export default function ExpandDescription({
   description,
   maxChars = 100,
-  isHtmlTemplate = false,
   ...props
 }: Props) {
   const isTooShort = description.length <= maxChars;
-  console.log({ isTooShort });
   const [isExpanded, setIsExpanded] = useState(false);
   const shortDescription = `${description.slice(0, maxChars)}...`;
 
   return (
-    <section {...props}>
-      {isTooShort ? (
-        isHtmlTemplate ? (
-          <div dangerouslySetInnerHTML={{ __html: description }} />
-        ) : (
-          description
-        )
-      ) : isExpanded ? (
-        isHtmlTemplate ? (
-          <div dangerouslySetInnerHTML={{ __html: description }} />
-        ) : (
-          description
-        )
-      ) : isHtmlTemplate ? (
-        <div dangerouslySetInnerHTML={{ __html: shortDescription }} />
-      ) : (
-        shortDescription
-      )}
+    <section className="flex flex-col gap-2">
+      <p {...props}>
+        {isTooShort || isExpanded ? description : shortDescription}
+      </p>
       {!isTooShort && (
         <Toggle
           variant="outline"
