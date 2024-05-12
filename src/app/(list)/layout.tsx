1f4c64384a088a81d10193f4ae9026b23bb19cf4
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-12 16:25:09 +0800

    Add list layout

---
+++ src/app/(list)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export default async function ListLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <main className="container max-w-5xl mx-auto min-h-screen px-2 py-4 md:py-8 md:px-4">
+      {children}
+    </main>
+  );
+}
