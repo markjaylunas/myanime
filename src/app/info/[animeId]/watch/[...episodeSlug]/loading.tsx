86bdcde9e87c25a38ede52b9ffe599ec428281eb
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-13 12:31:23 +0800

    Update loading

--- src/app/info/[animeId]/watch/[...episodeSlug]/loading.tsx
+++ src/app/info/[animeId]/watch/[...episodeSlug]/loading.tsx
@@ -2,10 +2,17 @@ import { Skeleton } from "@nextui-org/skeleton";
 
 export default function Loading() {
   return (
-    <section className="flex flex-col justify-center items-center">
+    <>
       <Skeleton className="w-full aspect-video" />
       <Skeleton className="rounded-xl w-full h-8 mt-2" />
-      <Skeleton className="rounded-xl max-w-60 w-full h-8 mt-4" />
-    </section>
+      <section className="flex justify-center items-center">
+        <Skeleton className="rounded-xl max-w-60 w-full h-8 mt-4" />
+      </section>
+
+      <section className="px-2 mt-6 space-y-2">
+        <Skeleton className="rounded-xl max-w-28 w-full h-8 mt-4" />
+        <Skeleton className="rounded-xl h-64 w-full" />
+      </section>
+    </>
   );
 }
