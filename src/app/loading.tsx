e27d524e0773093009bcd87e2cbc2cbfc8226f13
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-16 23:07:48 +0800

    add logo  to loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,11 @@
+import { Icons } from "@/components/ui/Icons";
 import { Spinner } from "@nextui-org/spinner";
 
 export default function Loading() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
+    <main className="min-h-screen flex flex-col gap-4 justify-center items-center">
       <Spinner size="lg" />
+      <Icons.logo className="size-24 text-primary" />
     </main>
   );
 }
