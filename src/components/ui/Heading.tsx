1d91840fc7dea9d639a8b30c5de707f9cdaa09eb
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-18 17:55:44 +0800

    update heading default size

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -7,7 +7,7 @@ const headingVariants = cva(
   {
     variants: {
       order: {
-        default: "text-3xl",
+        default: "text-2xl",
         "2xl": "text-2xl ",
         "3xl": "text-3xl",
         "4xl": "text-4xl",
