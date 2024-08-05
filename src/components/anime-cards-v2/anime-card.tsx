2e25ac9d92103d72e855c1b64f9a7aa66de7176b
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-08-05 17:57:09 +0800

    Add s2 anime list

--- src/components/anime-cards-v2/anime-card.tsx
+++ src/components/anime-cards-v2/anime-card.tsx
@@ -1,14 +1,16 @@
 import { AWAnimeSchema } from "@/lib/aniwatch-validations";
+import { cn } from "@/lib/utils";
 import { Card, CardFooter, CardHeader } from "@nextui-org/card";
 import { Chip } from "@nextui-org/chip";
 import { Image } from "@nextui-org/image";
 import Link from "next/link";
+import { Icons } from "../ui/Icons";
 
 export default function AnimeCard(anime: AWAnimeSchema) {
   return (
     <Card
       as={Link}
-      href={`s2/info${anime.id}`}
+      href={`s2/info/${anime.id}`}
       className="relative h-full w-full mx-auto aspect-2/3 bg-gray-600 select-none hover:cursor-pointer overflow-hidden"
     >
       <CardHeader className="absolute z-20 top-0 p-2 flex flex-wrap gap-2 justify-between items-start">
@@ -46,6 +48,36 @@ export default function AnimeCard(anime: AWAnimeSchema) {
             {anime.rating}
           </Chip>
         )}
+        <div className="flex justify-center items-center flex-wrap">
+          {Boolean(anime.episodes?.sub) && (
+            <Chip
+              radius="sm"
+              size="sm"
+              color="primary"
+              variant="shadow"
+              className={cn(
+                "mx-auto space-x-1",
+                anime.episodes?.dub && "rounded-r-none"
+              )}
+              startContent={<Icons.closedCaption />}
+            >
+              {anime.episodes?.sub}
+            </Chip>
+          )}
+
+          {Boolean(anime.episodes?.dub) && (
+            <Chip
+              radius="sm"
+              size="sm"
+              color="secondary"
+              variant="shadow"
+              className={cn("", anime.episodes?.sub && "rounded-l-none")}
+              startContent={<Icons.microphone />}
+            >
+              {anime.episodes?.dub}
+            </Chip>
+          )}
+        </div>
 
         <h6 className="text-white font-medium text-md line-clamp-2 text-center text-pretty">
           {anime.name}
