14c88c6f1d21fccde97d9e1af6fa26e0deae2440
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-08-07 12:15:20 +0800

    add anime list view for v2

---
+++ src/components/anime-cards-v2/anime-list.tsx
@@ -0,0 +1,19 @@
+import { AWAnimeSchema } from "@/lib/aniwatch-validations";
+import AnimeCardMotion from "../anime-cards/AnimeCardMotion";
+import AnimeCard from "./anime-card";
+
+type AnimeListProps = {
+  animeList: AWAnimeSchema[];
+};
+
+export default function AnimeList({ animeList }: AnimeListProps) {
+  return (
+    <ul className="grid grid-cols-2 xs:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
+      {animeList.map((anime, index) => (
+        <AnimeCardMotion isStaggered index={index} key={`${anime.id}-${index}`}>
+          <AnimeCard {...anime} />
+        </AnimeCardMotion>
+      ))}
+    </ul>
+  );
+}
