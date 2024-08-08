4998758ef48e23ddbff53243ce1c4f159b1504bf
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-08-09 01:10:45 +0800

    Add spotlight skeleton

--- src/app/s2/(home)/page.tsx
+++ src/app/s2/(home)/page.tsx
@@ -1,4 +1,5 @@
 import AnimeCarouselListSkeleton from "@/components/anime-cards/AnimeCarouselListSkeleton";
+import { Skeleton } from "@nextui-org/skeleton";
 import { Suspense } from "react";
 import ContinueList from "./_components/continue-list";
 import SortedList from "./_components/sorted-list";
@@ -7,7 +8,13 @@ import SpotlightList from "./_components/spotlight-list";
 export default async function Home() {
   return (
     <main className="space-y-8">
-      <Suspense fallback={<AnimeCarouselListSkeleton />}>
+      <Suspense
+        fallback={
+          <section className="max-w-5xl mx-auto px-0 sm:px-4 h-full w-full aspect-square sm:aspect-3/1">
+            <Skeleton className="h-full w-full" />
+          </section>
+        }
+      >
         <SpotlightList />
       </Suspense>
       <section className="container max-w-5xl mx-auto min-h-screen px-2 py-4 md:px-4 space-y-8">
